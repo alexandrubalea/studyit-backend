f2cd6994e52114c590c1b7bc00cc759c6350a2dd
Alexandru Balea <alexandru.simion013@gmail.com>
2019-11-11 19:09:58 +0200

    Added project

---
+++ src/main/java/com/ubbdevs/studyit/StudyitApplication.java
@@ -0,0 +1,13 @@
+package com.ubbdevs.studyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubbdevs/studyit/StudyitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubbdevs.studyit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
