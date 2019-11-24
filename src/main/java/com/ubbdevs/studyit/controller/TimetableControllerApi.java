c2776f6c4efe3ef7621bb766bd3f9a64cb716ff8
Alexandru Balea <alexandru.simion013@gmail.com>
2019-11-24 14:59:16 +0200

    [NONE] Set JDK version

--- src/main/java/com/ubbdevs/studyit/controller/TimetableControllerApi.java
+++ src/main/java/com/ubbdevs/studyit/controller/TimetableControllerApi.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Api(description = "Timetable API", tags = {"Timetable"})
 public interface TimetableControllerApi {
 
-    @ApiOperation(value = "Get the timetable for a group", response = TimetableEntry.class)
+    @ApiOperation(value = "Get the timetable for a group", response = TimetableEntryDto.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved timetable"),
             @ApiResponse(code = 400, message = "Invalid data")
